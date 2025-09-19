03c5f154a0963b2ae2160b0d6ce0f831f6ee8e20
Henrik Monteagle <henrik.browne@norgesgruppen.no>
2025-09-19 10:27:06 +0200

    Frontpage

---
+++ src/components/FrontPage/FrontPage.tsx
@@ -0,0 +1,40 @@
+import { Button, Box, Text } from "@chakra-ui/react";
+import bg from "../../assets/background_img.jpg";
+import "./FrontPage.css";
+import BuzzwordRain from "./BuzzWordRain";
+import PoweredByAi from "./PoweredByAI";
+
+const buzzwords = [
+  "Pikachu","Charizard","Bulbasaur","Squirtle","Mewtwo","Eevee","Snorlax",
+  "Gengar","Jigglypuff","Psyduck","Togepi","Dragonite","Lucario","Greninja",
+  "Gardevoir","Lapras","Onix","Machamp","Alakazam","Scyther","Magikarp",
+  "Ditto","Umbreon","Espeon","Sylveon","Chikorita","Cyndaquil","Totodile",
+  "Suicune","Entei","Raikou","Blaziken","Sceptile","Infernape","Garchomp",
+  "AI","Machine Learning","Neural Net","LLM","RAG","Vector","Inference"
+];
+
+export default function FrontPage() {
+  return (
+    <Box
+      position="relative"
+      minH="100vh"
+      bgImage={`url(${bg})`}
+      bgSize="cover"
+      backgroundPosition="center"
+      bgRepeat="no-repeat"
+    >
+      <BuzzwordRain words={buzzwords} count={50} maxDuration={16} color="whiteAlpha.800" zIndex={1} />
+
+      <Box className="front-page-content-container" position="relative" zIndex={2}>
+        <div className="front-page-central-content">
+          <Text className="front-page-central-text">
+            The next generation pokemon indexing
+          </Text>
+          <Button className="front-page-button">Experience</Button>
+        <PoweredByAi />
+
+        </div>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router";
+
+export function NavBar() {
+  return (
+    <nav>
+      <NavLink to="/" end>
+        Home
+      </NavLink>
+      <NavLink to="/trending" end>
+        Trending Concerts
+      </NavLink>
+      <NavLink to="/concerts">All Concerts</NavLink>
+      <NavLink to="/account">Account</NavLink>
+    </nav>
+  );
+}
