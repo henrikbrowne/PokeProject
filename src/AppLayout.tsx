20df71e2e427d324b94f17981bea668f9f8ad403
Henrik Monteagle <henrik.browne@norgesgruppen.no>
2025-09-19 10:48:20 +0200

    Added nav

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from "react-router";
 import FrontPage from "./components/FrontPage/FrontPage";
 import DetailsPage from "./views/DetailsPage";
 import Navbar from "./components/NavBar";
+import Game from "./components/Game/Game";
 
 function AppLayout() {
   const location = useLocation();
@@ -14,6 +15,7 @@ function AppLayout() {
       <Routes>
         <Route path="/" element={<FrontPage />} />
         <Route path="/details" element={<DetailsPage />} />
+        <Route path="/game" element={<Game />}></Route>
       </Routes>
     </>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,10 @@ import { NavLink } from "react-router";
 export default function NavBar() {
   return (
     <nav>
-      <NavLink to="/" end>
-        Home
-      </NavLink>
-      <NavLink to="/details" end>
-      </NavLink>
-      <NavLink to="/concerts">All Concerts</NavLink>
-      <NavLink to="/account">Account</NavLink>
+      <NavLink to="/" end>Home</NavLink>
+      <NavLink to="/details" end>Details</NavLink>
+      <NavLink to="/game">Game</NavLink>
+      <NavLink to="/PLACEHOLDER">PLACEHOLDER</NavLink>
     </nav>
   );
 }
