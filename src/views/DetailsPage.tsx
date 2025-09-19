5340144b490d3c753dd0774fd218c115bc844fd3
Eva <evateisb@gmail.com>
2025-09-19 11:37:58 +0200

    feat: add card background color based on type

--- src/views/DetailsPage.tsx
+++ src/views/DetailsPage.tsx
@@ -1,8 +1,8 @@
-import Card from "../components/card";
+import Card from "../components/Card";
 
 const DetailsPage = () => {
     return (
-        <Card name={"Name"} hp={"hp"} abilityNames={["ability1", "ability2"]} color={"lightblue"} type={"type"} image={"img"}></Card>
+        <Card name={"Name"} hp={"hp"} abilityNames={["ability1", "ability2"]} type={["fire"]} image={"img"}></Card>
     )
 }
 
