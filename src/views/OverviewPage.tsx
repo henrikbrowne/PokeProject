b945f49b1c3369930ae9a056978a5608a0443b78
Eirin Korvald <31256905+eirinko@users.noreply.github.com>
2025-09-19 14:21:11 +0200

    Added pokedex image without background.

--- src/views/OverviewPage.tsx
+++ src/views/OverviewPage.tsx
@@ -2,7 +2,7 @@
 import { useGetAllPokemonNamesQuery } from "../services/pokemon";
 import PokemonPreview from "../components/PokemonPreview/PokemonPreview";
 import { Box, Flex } from "@chakra-ui/react";
-
+import pokedex from '../assets/pokedex.png'
 
 const OverviewPage = () => {
     const { data, error, isLoading } = useGetAllPokemonNamesQuery();
@@ -11,26 +11,29 @@ const OverviewPage = () => {
 
     if (isLoading) return <p>Loading Pokémons...</p>;
     if (error) return <p>Oh no, something went wrong!</p>
+
+
     
     return (
         <>
-        <Flex backgroundImage={"url('https://cdn.dribbble.com/userupload/3875672/file/original-42f52449520e8e7940c668566888d84f.png?resize=1600x1200&vertical=center')"}
+
+        <Flex backgroundImage={`url(${pokedex})`}
             bgRepeat="no-repeat"
             bgSize="contain"
-            height="90vh"
+            height="100vh"
             width="100vw"
             backgroundPosition="center"
             display="flex"
             >
-            <Box maxWidth={"50rem"} maxHeight={"16rem"} 
-            overflowY="auto" scrollBehavior="smooth" 
-            position="absolute" scrollbar="hidden"
-            top="210px" left="645px"
-            >
-                {data?.results.map((pokemon) => (
-                <PokemonPreview key={pokemon.name} pokemonName={pokemon.name} />
-                ))} 
-            </Box>
+                <Box maxWidth={"50rem"} maxHeight={"17rem"} 
+                overflowY="auto" scrollBehavior="smooth" 
+                position="absolute" scrollbar="hidden"
+                top="305px" left="638px"
+                >
+                    {data?.results.map((pokemon) => (
+                    <PokemonPreview key={pokemon.name} pokemonName={pokemon.name} />
+                    ))} 
+                </Box>
         </Flex>
         </>
     )
