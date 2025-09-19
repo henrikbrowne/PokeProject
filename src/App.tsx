54cd91337b68ba867e100f8402fc0a41673a3c56
Eva <evateisb@gmail.com>
2025-09-19 09:48:17 +0200

    feat: add router and views

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Button } from '@chakra-ui/react/button'
 import './App.css'
-import FrontPage from './components/FrontPage/FrontPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import WelcomePage from './views/WelcomePage'
+import DetailsPage from './views/DetailsPage'
 
 function App() {
-
   return (
-    <>
-      <FrontPage />
-      <Button></Button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/details" element={<DetailsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
