6171ff2959f900593cec471a0cea98723c5112b5
Eva <evateisb@gmail.com>
2025-09-19 16:14:05 +0200

    fix: visually indicate active page in navbar

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 import './Navbar.css';
 import facebookIcon from "../../assets/facebook.svg";
 import instagramIcon from "../../assets/instagram.png";
@@ -7,11 +7,24 @@ import youtubeIcon from "../../assets/youtube.png";
 import whatsappIcon from "../../assets/whatsapp.png";
 
 export default function NavBar() {
+  const location = useLocation();
+
+    const isPokedexActive = location.pathname.startsWith("/overview") || location.pathname.startsWith("/details");
+
   return (
     <nav className="navbar">
-      <NavLink to="/" end>Home</NavLink>
-      <NavLink to="/overview">Pokédex</NavLink>
-      <NavLink to="/game">Game</NavLink>
+      <NavLink to="/" end className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+        Home
+      </NavLink>
+
+      <NavLink to="/overview" className={() => isPokedexActive ? "nav-link active" : "nav-link"}>
+        Pokédex
+      </NavLink>
+
+      <NavLink to="/game" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+        Game
+      </NavLink>
+      
       <NavLink to="/" end>
         <img src={facebookIcon} alt="Home" className="nav-icon" />
       </NavLink>
