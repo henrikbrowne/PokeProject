9ee0a51dd86eb73947a647d9b245527cded98adb
Henrik Monteagle <henrik.browne@norgesgruppen.no>
2025-09-22 13:15:43 +0200

    Sped up pokemon loading with hacky fix

--- src/components/PokeomatResult/PokeomatResult.tsx
+++ src/components/PokeomatResult/PokeomatResult.tsx
@@ -1,6 +1,5 @@
 import { useGetPokemonByNameQuery } from "../../services/pokemon";
 import { Image } from '@chakra-ui/react'
-import { useEffect, useState } from "react";
 
 type PokeomatResultProps = { pokemon:string, flufftext:string };
 
@@ -8,15 +7,11 @@ type PokeomatResultProps = { pokemon:string, flufftext:string };
 const PokeomatResult = ({ pokemon, flufftext }: PokeomatResultProps) => {
   const { data, error, isLoading } = useGetPokemonByNameQuery(pokemon)
   const url = data?.sprites.front_default ?? "";
-
-  const [showImage, setShowImage] = useState(false);
-
-
    
 
   return (
     <>
-    {showImage && <Image rounded="md" src={url} alt="Dan Abramov" />}
+    {url && <Image rounded="md" src={url} alt="Dan Abramov" />}
     <h1>Your Pokémon: {pokemon}</h1>
     <h1>{flufftext}</h1>
     </>
