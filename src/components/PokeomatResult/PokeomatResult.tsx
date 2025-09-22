43a26fc0399c8b6a6eb121ce6b01444cab9e9f4b
Henrik Monteagle <henrik.browne@norgesgruppen.no>
2025-09-22 11:27:35 +0200

    Added pokemoat result

--- src/components/PokeomatResult/PokeomatResult.tsx
+++ src/components/PokeomatResult/PokeomatResult.tsx
@@ -1,11 +1,32 @@
+import { useGetPokemonByNameQuery } from "@/services/pokemon";
+import { Image } from '@chakra-ui/react'
+import { useEffect, useState } from "react";
+
 type PokeomatResultProps = { pokemon:string, flufftext:string };
 
 
 const PokeomatResult = ({ pokemon, flufftext }: PokeomatResultProps) => {
+  const { data, error, isLoading } = useGetPokemonByNameQuery(pokemon)
+  const url = data?.sprites.front_default ?? "";
+
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowImage(true);
+    }, 2000);
+
+    // cleanup if component unmounts early
+    return () => clearTimeout(timer);
+  }, []);
+
+   
+
   return (
     <>
-      <h1>Your Pokémon: {pokemon}</h1>
-      <h1>{flufftext}</h1>
+    {showImage && <Image rounded="md" src={url} alt="Dan Abramov" />}
+    <h1>Your Pokémon: {pokemon}</h1>
+    <h1>{flufftext}</h1>
     </>
   );
 };
